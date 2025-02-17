00562b09268b9ab875f2cbb6e101b261a5cf2329
Md Sahin Alam <sahinraj20@gmail.com>
2025-02-17 19:42:13 +0600

    initial setup

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React, { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './routes/Router'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </StrictMode>,
+)
