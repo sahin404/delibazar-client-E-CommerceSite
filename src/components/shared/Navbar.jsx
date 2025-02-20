1d8405b494f47bcfb5daf69227e4567ec8f80811
Md Sahin Alam <sahinraj20@gmail.com>
2025-02-21 00:35:43 +0600

    navbar runnig

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,39 +1,47 @@
 import { FaRegUser } from "react-icons/fa"
 import { FiShoppingCart } from "react-icons/fi"
-import { IoCallOutline, IoSearchOutline } from "react-icons/io5"
+import { IoCallOutline, IoLanguageSharp, IoSearchOutline } from "react-icons/io5"
 import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
     <div className="bg-[#A31D1D] flex items-center justify-center py-3">
-      <Link to="/">
-        <img src="logo-1.png" className="h-16" />
-      </Link>
+      {/* Left */}
+      <div>
+        <Link to="/">
+          <img src="logo-1.png" className="h-16" />
+        </Link>
+      </div>
 
+
+      {/* Middle */}
       <div className="flex items-center border-none">
         <input type="text" placeholder="প্রোডাক্ট সার্চ করুন" className="input rounded-r-none rounded-l-xl h-11 w-80 text-sm focus:none" />
         <button className="bg-[#FFD814] h-11 p-2 rounded-r-xl">
           <IoSearchOutline className="text-3xl " />
         </button>
       </div>
-      <div>
-        <div className="relative w-fit opacity-80">
-          <FiShoppingCart className="text-2xl text-white" />
-          <span className="absolute -right-1 -top-2 flex size-5 items-center justify-center rounded-full bg-[#FFD814] text-center text-[10px] text-black">12</span>
+      {/* Right Side */}
+      <div className="flex gap-3 items-center">
+        <div>
+          <div className="relative w-fit opacity-80 cursor-pointer">
+            <FiShoppingCart className="text-2xl text-white" />
+            <span className="absolute -right-1 -top-2 flex size-5 items-center justify-center rounded-full bg-[#FFD814] text-center text-[10px] text-black">12</span>
+          </div>
         </div>
-      </div>
-      <div className="flex items-center text-white opacity-80 gap-1 border p-2 rounded-xl">
-        <IoCallOutline className="text-2xl" />
-        <h1>যোগাযোগ</h1>
-      </div>
-      <div className="border p-2 rounded-xl text-white text-sm flex items-center gap-1 opacity-80">
-        <FaRegUser className="text-xl" />
-        <div>লগইন / নিবন্ধন</div>
+        <div className="flex items-center text-white/90 gap-1 border border-white/50 p-2 rounded-xl text-sm cursor-pointer hover:bg-white/30 duration-100">
+          <IoLanguageSharp className="text-xl" />
+          <h1>ভাষা</h1>
+        </div>
+        <div className="flex items-center text-white/90 gap-1 border border-white/50 p-2 rounded-xl text-sm hover:bg-white/30 duration-100 cursor-pointer">
+          <IoCallOutline className="text-xl" />
+          <h1>যোগাযোগ</h1>
+        </div>
+        <div className="border p-2 rounded-xl text-white/90 gap-1 border-white/50 text-sm flex items-center hover:bg-white/30 duration-100 cursor-pointer">
+          <FaRegUser className="text-xl" />
+          <div>লগইন / নিবন্ধন</div>
 
-      </div>
-
-      <div>
-
+        </div>
       </div>
 
     </div>
