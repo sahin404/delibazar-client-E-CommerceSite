0154afff796d51e3a1ea87d0768276566d5274e3
Md Sahin Alam <sahinraj20@gmail.com>
2025-03-18 03:44:24 +0600

    add login, logout, state change feature

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,11 +1,46 @@
-import { createContext, useState } from "react"
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { createContext, useEffect, useState } from "react"
 export const AuthContext = createContext(null);
+const auth = getAuth();
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
   const [user, setUser] = useState([]);
   const [loading, setLoading] = useState(true);
+
+
+  const signUp = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+
+  }
+
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  }
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      if (currentUser) {
+        setLoading(false);
+        console.log(currentUser);
+      }
+    })
+    return () => {
+      return unSubscribe();
+    }
+  }, [])
+
+
+
   const info = {
-    user, loading
+    user, loading, signUp, signIn, logOut
   }
   return (
     <AuthContext.Provider value={info}>
