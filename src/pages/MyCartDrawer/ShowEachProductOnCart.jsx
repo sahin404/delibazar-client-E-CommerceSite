e7472cc011fad048515e9c2f0e198ca23cdb1241
Md Sahin Alam <sahinraj20@gmail.com>
2025-03-21 14:33:14 +0600

    designed the drawer carts

--- src/pages/MyCartDrawer/ShowEachProductOnCart.jsx
+++ src/pages/MyCartDrawer/ShowEachProductOnCart.jsx
@@ -1,8 +1,30 @@
-const ShowEachProductOnCart = ({product}) => {
-    const {name,id, price, image} = product;
-  return (
-    <div></div>
-  )
+
+import { MdDelete } from "react-icons/md";
+import { Link } from "react-router-dom";
+
+const ShowEachProductOnCart = ({ product }) => {
+    const { name, id, price, picture } = product;
+    return (
+        <div>
+            
+            <div className=" flex justify-between items-center gap-3">
+            <div className="flex items-center gap-3">
+                <div className=" w-14 h-14 border flex items-center">
+                    <img src={picture} alt="" />
+                </div>
+                <div className="col-span-3">
+                    <Link to={`/product/${id}`}>  <h1 className="hover:underline">{name}</h1></Link>
+                    <h1>{price.toLocaleString("bn-BD")} ৳</h1>
+                </div>
+            </div>
+
+            <div className="text-2xl">
+                <button className="text-red-700"><MdDelete /></button>
+            </div>
+        </div>
+        <hr />
+        </div>
+    )
 }
 
 export default ShowEachProductOnCart
