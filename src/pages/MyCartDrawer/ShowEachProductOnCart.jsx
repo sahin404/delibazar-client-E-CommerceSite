294bb3113620a8ea9d2d33dd562bf6637f21e8aa
Md Sahin Alam <sahinraj20@gmail.com>
2025-03-21 14:53:02 +0600

    fetching cart delete api

--- src/pages/MyCartDrawer/ShowEachProductOnCart.jsx
+++ src/pages/MyCartDrawer/ShowEachProductOnCart.jsx
@@ -1,28 +1,41 @@
 
 import { MdDelete } from "react-icons/md";
 import { Link } from "react-router-dom";
+import useAxiosSecure from "../../hooks/useAxiosPublic/useAxiosSecure";
+import useCarts from "../../hooks/useCarts/useCarts";
 
 const ShowEachProductOnCart = ({ product }) => {
-    const { name, id, price, picture } = product;
+    const { name, id, price, picture, _id } = product;
+    const axiosSecure = useAxiosSecure();
+    const [, refetch] = useCarts();
+
+    const handleCartDelete = () => {
+        axiosSecure.delete(`/cart/${_id}`)
+            .then(res => {
+                if (res.data.deletedCount > 0) {
+                    refetch();
+                }
+            })
+    }
     return (
         <div>
-            
-            <div className=" flex justify-between items-center gap-3">
-            <div className="flex items-center gap-3">
-                <div className=" w-14 h-14 border flex items-center">
-                    <img src={picture} alt="" />
-                </div>
-                <div className="col-span-3">
-                    <Link to={`/product/${id}`}>  <h1 className="hover:underline">{name}</h1></Link>
-                    <h1>{price.toLocaleString("bn-BD")} ৳</h1>
-                </div>
-            </div>
 
-            <div className="text-2xl">
-                <button className="text-red-700"><MdDelete /></button>
+            <div className=" flex justify-between items-center gap-3">
+                <div className="flex items-center gap-3">
+                    <div className=" w-14 h-14 border flex items-center">
+                        <img src={picture} alt="" />
+                    </div>
+                    <div className="col-span-3">
+                        <Link to={`/product/${id}`}>  <h1 className="hover:underline">{name}</h1></Link>
+                        <h1>{price.toLocaleString("bn-BD")} ৳</h1>
+                    </div>
+                </div>
+
+                <div className="text-2xl">
+                    <button onClick={handleCartDelete} className="text-red-700"><MdDelete /></button>
+                </div>
             </div>
-        </div>
-        <hr />
+            <hr />
         </div>
     )
 }
