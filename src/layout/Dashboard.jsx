00c3b7f60d9b0eeaf32d49300e82e91bdd267508
Md Sahin Alam <sahinraj20@gmail.com>
2025-04-09 09:12:26 +0600

    designing the dashboard layout

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -1,9 +1,28 @@
+import { FaHome } from "react-icons/fa"
+import { Link, NavLink, Outlet } from "react-router-dom"
+
 const Dashboard = () => {
   return (
-    <div>
-      {/* adding dashboard component */}
-      
-    </div>
+    <div className="flex gap-10">
+            <div className="bg-[#D1A054] w-64 min-h-screen " >
+                <ul className="menu text-xl">
+                  <div>
+                    <Link>Add products</Link>
+                    <Link>Update Product</Link>
+                    <Link>Delete products</Link>
+                  </div>
+                    <div className="divider"></div>
+                    <li><NavLink to="/">
+                        <FaHome></FaHome>
+                        Go to Home
+                    </NavLink></li>
+                </ul>
+
+            </div>
+            <div className="flex-1 mt-10">
+                <Outlet></Outlet>
+            </div>
+        </div>
   )
 }
 
