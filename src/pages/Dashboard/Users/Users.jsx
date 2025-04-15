30def1b3a583076b3f782dba660b37ce6827f189
Md Sahin Alam <sahinraj20@gmail.com>
2025-04-16 02:56:17 +0600

    user list page setup

--- src/pages/Dashboard/Users/Users.jsx
+++ src/pages/Dashboard/Users/Users.jsx
@@ -4,22 +4,56 @@ import Spinner from "../../../components/shared/Spinner";
 
 const Users = () => {
   const axiosSecure = useAxiosSecure();
-  const fetchingData = async ()=>{
+  const fetchingData = async () => {
     const res = await axiosSecure.get('/users')
     return res.data;
   }
- 
-  const {data:users=[], isLoading} = useQuery({
-    queryKey:['users'],
+
+  const { data: users = [], isLoading } = useQuery({
+    queryKey: ['users'],
     queryFn: fetchingData
   })
 
-  if(isLoading){
+  if (isLoading) {
     return <Spinner></Spinner>
   }
   return (
     <div>
+      <h1 className="text-red-500 font-semibold text-3xl text-center">All Users List</h1>
+      <div className="mt-5">
 
+        <table className="table-auto w-full border-collapse">
+          <thead>
+            <tr className="bg-[#D1A054] text-white">
+              <th className="px-4 py-2">Serial No</th>
+              <th className="px-4 py-2">Name</th>
+              <th className="px-4 py-2">Email</th>
+              <th className="px-4 py-2">Creation Time</th>
+              <th className="px-4 py-2">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user._id} className="border-b">
+                <td className="px-4 py-2">{index + 1}</td>
+                <td className="px-4 py-2">{user.name}</td>
+                <td className="px-4 py-2">{user.email} BDT</td>
+                <td className="px-4 py-2">{user.date}</td>
+                <td className="px-4 py-2 flex gap-2">
+                  <button className="bg-blue-500 text-white px-4 py-1 rounded">
+                    Make Admin
+                  </button>
+                  <button className="bg-red-500 text-white px-4 py-1 rounded">
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+
+      </div>
     </div>
   )
 }
