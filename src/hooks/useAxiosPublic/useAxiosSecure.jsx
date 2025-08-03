406eb3304f9f53c2425db130f927a98dd3d954a0
Md. Sahin Alan <sahinraj20@gmail.com>
2025-08-03 22:18:47 +0600

    update axios

--- src/hooks/useAxiosPublic/useAxiosSecure.jsx
+++ src/hooks/useAxiosPublic/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosSecure = axios.create({
-  baseURL: 'https://delibazar-server-e-commerce-site.vercel.app/'
+  baseURL: 'https://delibazar-server.onrender.com'
 })
 
 const useAxiosSecure = () => {
