6ece84ad962864d12b5f73db34f89deb2af5d8dc
Md. Sahin Alan <sahinraj20@gmail.com>
2025-08-06 21:49:32 +0600

    server link updated

--- src/hooks/useAxiosPublic/useAxiosSecure.jsx
+++ src/hooks/useAxiosPublic/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosSecure = axios.create({
-  baseURL: 'http://localhost:3000/'
+  baseURL: 'https://delibazar-server.onrender.com/'
 })
 
 axiosSecure.interceptors.request.use(
